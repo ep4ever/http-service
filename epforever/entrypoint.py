954cdd47f2e56c3cea0e66e4a9de661fced34642
victor <victor@ep4ever.com>
2023-07-11 06:46:30 +0200

    removed argparse capabilities - no need to run a subprocess

--- epforever/entrypoint.py
+++ epforever/entrypoint.py
@@ -1,7 +1,5 @@
 import signal
 import sys
-import subprocess
-import argparse
 from dotenv import dotenv_values
 from epforever.ep_http_server import EpHttpServer
 
@@ -17,37 +15,10 @@ def exit_now(signum, frame):
 
     sys.exit(0)
 
-
-def run_config(config):
-    print('todo: ask user for editing .env file')
-    pass
-
-
 def start():
     global proc
 
-    parser = argparse.ArgumentParser(
-        description='Optional ep4ever http service arguments'
-    )
-    parser.add_argument(
-        '--config',
-        action='store_true',
-        help='If provided will start the configuration wizard'
-    )
-    parser.add_argument(
-        '--withserial',
-        action='store_true',
-        help='If provided will start the serial service in a sub process'
-    )
-    args = parser.parse_args()
     config = dotenv_values(".env")
-
-    if args.config:
-        return run_config(config)
-
-    if args.withserial:
-        serial_service = config.get('SERIAL_SERVICE_PATH')
-        proc = subprocess.Popen([serial_service])
 
     server = EpHttpServer(config)
     signal.signal(signal.SIGINT, exit_now)
