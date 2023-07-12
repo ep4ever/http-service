25c2afe814c5a0bdaf0f322fc587f3f5bb7985c8
victor <victor@ep4ever.com>
2023-07-12 07:05:39 +0200

    removed unused variable

--- epforever/entrypoint.py
+++ epforever/entrypoint.py
@@ -3,21 +3,11 @@ import sys
 from dotenv import dotenv_values
 from epforever.ep_http_server import EpHttpServer
 
-proc = None
-
-
 def exit_now(signum, frame):
-    global proc
     print("\nShuting down server...")
-    if proc is not None:
-        print("\nShuting down serial service...")
-        proc.kill()
-
     sys.exit(0)
 
 def start():
-    global proc
-
     config = dotenv_values(".env")
 
     server = EpHttpServer(config)
